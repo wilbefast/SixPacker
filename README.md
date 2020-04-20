b2d7bf901c74e885485506b226aacad612e32331
William Dyce <wilbefast@gmail.com>
2020-04-20 21:56:34 +0200

    initial version

---
+++ README.md
@@ -0,0 +1,21 @@
+# SIX PACKER
+
+## WHAT IT IS
+This is a little sprite packing script I wrote for [Heaps](https://heaps.io), but it will work for anything that uses the [.atlas](https://fileinfo.com/extension/atlas) format.
+
+## DEPENDENCIES
+You'll need to install [ImageMagick](https://imagemagick.org).
+
+## HOW TO USE IT
+You can regenerate the example by doing the following:
+```bash
+$cd example
+../sixpacker.sh foreground
+```
+This will generate an atlas in PNG format as well as metadata in ALTAS format. Each folder is assumed to contain an animated, exported as individual frames. I use [Krita](https://krita.org/)'s "render animation" to create these frames easily.
+
+## ROADMAP
+I plan to improve the tool moving forward so that it packs atlases better: currently it is very much a minimal viable product for use in small projects.
+
+## LICENCE
+I use the MIT licence. Please mention me, but otherwise you're free to use this however you like. 
---
+++ sixpacker.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+
+# Marshall all the variable we'll need
+OUTPUT=$1
+ATLAS=${OUTPUT}.atlas
+PNG=${OUTPUT}.png
+
+# Clean up any pre-existing stuff
+rm -f ${ATLAS}
+rm -f ${PNG}
+
+# Turn each folder into a single horizontal animation strip
+for folder in */ ; do
+  montage ${folder}*.png -tile x1 -geometry +0+0 ${folder%%/*}.png
+done
+
+# Stack the strips on top of each other to build the atlas
+montage *.png -tile 1x -geometry +0+0 ${PNG}
+PNG_WIDTH=$(convert ./${PNG} -format "%w" info:)
+PNG_HEIGHT=$(convert ./${PNG} -format "%h" info:)
+
+# Write the atlas file's header information
+touch ${ATLAS}
+echo ${PNG} >> ${ATLAS}
+echo "size: ${PNG_WIDTH},${PNG_HEIGHT}" >> ${ATLAS}
+echo "format: RGBA8888" >> ${ATLAS}
+echo "filter: Linear,Linear" >> ${ATLAS}
+echo "repeat: none" >> ${ATLAS}
+
+OFFSET_Y=0
+for folder in */ ; do
+  rm ${folder%%/*}.png
+  OFFSET_X=0
+  INDEX=0
+  for image in ${folder}*.png ; do 
+    WIDTH=$(convert ./${image} -format "%w" info:)
+    HEIGHT=$(convert ./${image} -format "%h" info:)
+    echo ${folder%%/*} >> ${ATLAS}
+    echo " rotate: false" >> ${ATLAS}
+    echo " xy: 0, 0" >> ${ATLAS}
+    echo " size: ${WIDTH}, ${HEIGHT}" >> ${ATLAS}
+    echo " orig: 0, 0" >> ${ATLAS}
+    echo " offset: ${OFFSET_X}, ${OFFSET_Y}" >> ${ATLAS}
+    echo " index: ${INDEX}" >> ${ATLAS}
+    ((OFFSET_X+=WIDTH))
+    ((INDEX++))
+  done
+  ((OFFSET_Y+=HEIGHT))
+done
